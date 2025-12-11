0940983230240f825ae5e8e85fb85b414e954636
Ingo Wolf <me@ingowolf.au>
2025-12-11 19:25:22 +1100

    add admin redirect thing

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,13 @@
-export default function AdminPage() {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function AdminPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/admin/login");
+  }
+
   return (
     <div>
       <h1>Admin Page</h1>
