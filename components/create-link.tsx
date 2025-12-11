3779f6c412d9b59cc3bfbe79ff8f035e29b50cbd
Ingo Wolf <me@ingowolf.au>
2025-12-12 10:57:00 +1100

    move form to component

--- components/create-link.tsx
+++ components/create-link.tsx
@@ -9,13 +9,19 @@ export default function CreateLink() {
   return (
     <div className="border p-2 flex flex-col gap-2">
       <div>create</div>
-      <form className="flex flex-row gap-2" action={() => links.create()}>
-        <Input placeholder="link" name="link" required autoFocus />
-        <Input placeholder="path" name="path" />
-        <Button variant="outline">
-          Create<Kbd>⏎</Kbd>
-        </Button>
-      </form>
+      <Form />
     </div>
+  );
+}
+
+function Form() {
+  return (
+    <form className="flex flex-row gap-2" action={() => links.create()}>
+      <Input placeholder="link" name="link" required autoFocus />
+      <Input placeholder="path" name="path" />
+      <Button variant="outline">
+        Create<Kbd>⏎</Kbd>
+      </Button>
+    </form>
   );
 }
