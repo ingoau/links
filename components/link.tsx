e53d86408f3d95cbfa582d40ee96dd3565d5df6f
Ingo Wolf <me@ingowolf.au>
2025-12-12 12:34:44 +1100

    add links

--- components/link.tsx
+++ components/link.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import { Link } from "@/lib/types";
+import { Link as LinkType } from "@/lib/types";
 import { Button } from "./ui/button";
 import { Pencil, Trash } from "lucide-react";
+import Link from "next/link";
 
-export default function LinkComponent({ link }: { link: Link }) {
+export default function LinkComponent({ link }: { link: LinkType }) {
   return (
     <div className="border flex flex-row p-2 items-center">
       <div className="flex flex-col grow">
-        <h2 className="text-lg">/{link.path}</h2>
-        <h3 className="text-sm text-neutral-400">{link.link}</h3>
+        <Link
+          href={`/${link.path}`}
+          target="_blank"
+          className="hover:underline"
+        >
+          <h2 className="text-lg">/{link.path}</h2>
+        </Link>
+        <Link href={link.link} target="_blank" className="hover:underline">
+          <h3 className="text-sm text-neutral-400">{link.link}</h3>
+        </Link>
         <h3 className="text-sm text-neutral-400">
           {new Date(link.createdAt).toLocaleString()}
         </h3>
