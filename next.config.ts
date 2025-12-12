e3fe126e644ab7527936e8ca936a7e02d53b56b5
Ingo Wolf <me@ingowolf.au>
2025-12-12 11:36:18 +1100

    remove github avatars from trusted image urls

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
