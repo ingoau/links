ff60bc19d36f5788bb75b55539599c70ef130148
Ingo Wolf <me@ingowolf.au>
2025-12-19 13:35:06 +1100

    move toast again

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <Toaster position="top-center" />
+          <Toaster position="top-right" />
         </ThemeProvider>
       </body>
     </html>
