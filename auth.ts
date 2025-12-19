bc734d601269a278c7d1d26e71a8293a2694d7d6
Ingo Wolf <me@ingowolf.au>
2025-12-19 14:18:17 +1100

    add auth based on email

--- auth.ts
+++ auth.ts
@@ -3,4 +3,16 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async signIn({ profile }) {
+      if (!process.env.ALLOWED_EMAIL) {
+        return false;
+      }
+      if (profile?.email == process.env.ALLOWED_EMAIL) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 });
